845ff01dc7d5ffb2b872859848fe6cd7ef75e422
inderpreet2 <inderpreetbkaur@gmail.com>
2022-07-26 11:47:04 +0530

    Create Inderpreet.java

---
+++ Inderpreet.java
@@ -0,0 +1,60 @@
+//Ques 1
+class Test2{
+public static void main(String[]inderpreetkaur){
+
+int a age=20;
+System.out.println("My age is="+age);
+
+}
+Test2.obj=new Test2();
+obj.hobby();
+
+
+{
+	static int age(int d)
+	return d;
+}
+	void hobby(){
+	
+
+
+System.out.println("My hobby is dancing");
+}
+}
+
+/ Ques 2
+Class Name= ClassMethods
+Function Name=classMethods()
+Variable=classmethods
+Static Variable= CLASS_METHODS
+
+QUES 3
+
+1. A java class file containing a java bytecode and having .class extension. A java class file is created by a java compiler form and .class file is the file after the compilation of the program.
+2. OOP'S Programming is not a complete java because we use different types of data types in the program and we can't able to make a objects in this. So, java is not a pure or complete OOP'S programming.
+3. NR-WA(No retun and with argument)
+   void subtract(int a, int b)
+   void is no return type and (int a,int b) is a arguement.
+   
+QUES 5
+
+Java is not a complete OOP'S programming langauge. Because we use different types of data types in the program and so java is used to overcome this with the help of wrapper classes.
+For eg.
+class DataTypes{
+	
+	public static void main(String[]a){
+		Interger i=20;
+		System.out.println("value of integer="+i);
+	}
+}
+Ques 4
+
+class Calci
+public static void main(String[]args){
+	
+	Calci.obj=new Calci();
+	
+	
+	
+	
+}
